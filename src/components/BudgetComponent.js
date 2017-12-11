6d0fd715c59c308d83bd3639405e4787687151e5
Phat Thaveepholcharoen <overtone00445@gmail.com>
2017-12-11 16:40:13 +0700

    Bind the budget value to its state

--- src/components/BudgetComponent.js
+++ src/components/BudgetComponent.js
@@ -34,7 +34,7 @@ export default class BudgetComponent extends Component {
                 <View style={{paddingVertical: 10}}>
                     <View style={{justifyContent: 'center', alignItems: 'center'}}>
                         <Text style={local.budgetVal}>{this.state.budgetValue}</Text></View>
-                    <Slider minimumValue={0} maximumValue={40000} step={500} value={5000}
+                    <Slider minimumValue={0} maximumValue={40000} step={500} value={this.state.budgetValue}
                         thumbTintColor={Color.secondary} maximumTrackTintColor={Color.secondaryLight}
                         minimumTrackTintColor={Color.secondaryLight}
                         onValueChange={(value) => this.setState({budgetValue: value})}></Slider>
